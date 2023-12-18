f7d0fb150de708938c9f929e976586b4945d1bde
= <=>
2023-12-18 17:53:00 +0530

    some file

---
+++ src/main/java/com/example/filedataapis/FileDataApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.filedataapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDataApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDataApisApplication.class, args);
+	}
+
+}
